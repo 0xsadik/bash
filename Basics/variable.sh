d749be2834f3bdd91e4b588fbe15d817d70e6db6
0xsadik <torikussadik404@gmail.com>
2023-12-17 17:16:05 +0600

    basics

---
+++ Basics/variable.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# price_per_apple = 5
+# echo "The price of an apple today is: ${price_per_apple}"
+
+
+# ls
+
+# name="torikus sadik"
+# echo $name
+
+
+
+#readonly
+
+
+# name="torikus"
+# readonly name
+# name="sadik"
+
+
+#unset 
+
+# name="torikus sadik"
+# unset name
+# echo $name
+
+
+# echo "File name : $0"
+# echo "First parameter: $1"
+# echo "Second parameter: $2"
+# echo "Quoted values: $@"
+# echo "Total number of parameters: $#"
+
+
+
+# for TOKEN in $*
+# do 
+#     echo $TOKEN
+# done
+
+
+# ------ tryhackme -------
+
+
+# name="jammy"
+# echo $name
+
+
+
+# multiple variable 
+
+# name="jammy"
+# age=21
+# echo "$name is $age years old"
+
+
+
